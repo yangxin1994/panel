3d6216b5e8e17a27e18d2862bfb4da45887fc11a
t ddddddd <webdev@cmme.org>
2016-04-06 09:20:38 -0700

    add example app

---
+++ example/index.js
@@ -0,0 +1,48 @@
+import { App, View } from '../build';
+import aboutTemplate from './about.jade';
+import counterTemplate from './counter.jade';
+
+class CounterApp extends App {
+  get ROUTES() {
+    return {
+      'counter': this.counter,
+      'about':   this.about,
+      '':        this.about,
+    };
+  }
+
+  get SCREENS() {
+    return {
+      counter: new CounterView(),
+      about:   this.viewFromTemplate(aboutTemplate),
+    };
+  }
+
+  about() {
+    return {$screen: 'about'};
+  }
+
+  counter() {
+    return {$screen: 'counter'};
+  }
+
+  changeCounter(offset) {
+    this.update({counter: this.state.counter + offset});
+  }
+}
+
+class CounterView extends View {
+  get TEMPLATE() {
+    return counterTemplate;
+  }
+
+  get templateHandlers() {
+    return {
+      incr: () => this.app.changeCounter(1),
+      decr: () => this.app.changeCounter(-1),
+    }
+  }
+}
+
+const app = new CounterApp('app', {$screen: 'about', counter: 1});
+app.navigate(window.location.hash);
---
+++ example/webpack.config.js
@@ -0,0 +1,46 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+var webpack = require('webpack');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var webpackConfig = {
+  entry: './index.js',
+  module: {
+    loaders: [
+      {
+        test: /\.jade$/,
+        exclude: /node_modules/,
+        loader: 'virtual-jade',
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+            presets: ['es2015'],
+        },
+      },
+      {
+        test: /\.(png|svg)$/,
+        loader: 'url-loader?limit=50000',
+      },
+      {
+        test: /\.styl$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!stylus-loader'),
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      APP_ENV: JSON.stringify(process.env.NODE_ENV),
+    }),
+    new HtmlWebpackPlugin({template: 'index.template.html'}),
+  ],
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
+  },
+};
+
+module.exports = webpackConfig;
