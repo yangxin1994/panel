75d8da7aa755067a1f6ca79da2fa65cc43be6275
t ddddddd <webdev@cmme.org>
2016-04-09 16:24:31 -0700

    super basic rendering test

---
+++ test/index.js
@@ -0,0 +1,25 @@
+const document = require('min-document');
+const expect = require('expect.js');
+const h = require('virtual-dom/virtual-hyperscript');
+const panel = require('../build');
+
+const windowStub = {
+  addEventListener: () => {},
+  history: {},
+  location: {},
+};
+
+class SampleApp extends panel.App {
+  get SCREENS() {
+    return {hello: this.viewFromTemplate(state => h('div.hello'))};
+  }
+}
+
+describe('panel (basic usage)', () => {
+  it('renders to DOM', () => {
+    const el = document.createElement('div');
+    new SampleApp(el, {$screen: 'hello'}, {window: windowStub}).start();
+    expect(el.childNodes).to.have.length(1);
+    expect(el.childNodes[0].className).to.eql('hello');
+  });
+});
