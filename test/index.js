4ad66cd48351d45cea34cb7d48ebc3fe76e00963
t ddddddd <webdev@cmme.org>
2016-04-09 16:28:21 -0700

    test specifying when first render occurs

--- test/index.js
+++ test/index.js
@@ -16,10 +16,16 @@ class SampleApp extends panel.App {
 }
 
 describe('panel (basic usage)', () => {
-  it('renders to DOM', () => {
+  it('renders to DOM after start() called', () => {
     const el = document.createElement('div');
     new SampleApp(el, {$screen: 'hello'}, {window: windowStub}).start();
     expect(el.childNodes).to.have.length(1);
     expect(el.childNodes[0].className).to.eql('hello');
+  });
+
+  it('does not render to DOM until start() called', () => {
+    const el = document.createElement('div');
+    new SampleApp(el, {$screen: 'hello'}, {window: windowStub});
+    expect(el.childNodes).to.have.length(0);
   });
 });
