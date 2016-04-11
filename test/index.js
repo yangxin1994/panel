e31832f1ca83c2a0edd89333dace8c15d9ba6a8c
t ddddddd <webdev@cmme.org>
2016-04-10 18:17:59 -0700

    test context reorg

--- test/index.js
+++ test/index.js
@@ -9,39 +9,43 @@ const windowStub = {
   location: {},
 };
 
-class ConstantApp extends App {
-  get SCREENS() {
-    return {hello: this.viewFromTemplate(state => h('div.hello'))};
-  }
-}
-
-class StatefulApp extends App {
-  get SCREENS() {
-    return {hello: this.viewFromTemplate(state => h('div.animal', `Hello ${state.animal}`))};
-  }
-}
-
 describe('renderer', () => {
-  it('updates DOM after start() called', () => {
-    const el = document.createElement('div');
-    new ConstantApp(el, {$screen: 'hello'}, {window: windowStub}).start();
-    expect(el.childNodes).to.have.length(1);
-    expect(el.childNodes[0].className).to.eql('hello');
+  context('in basic usage', () => {
+    class ConstantApp extends App {
+      get SCREENS() {
+        return {hello: this.viewFromTemplate(state => h('div.hello'))};
+      }
+    }
+
+    it('updates DOM after start() called', () => {
+      const el = document.createElement('div');
+      new ConstantApp(el, {$screen: 'hello'}, {window: windowStub}).start();
+      expect(el.childNodes).to.have.length(1);
+      expect(el.childNodes[0].className).to.eql('hello');
+    });
+
+    it('does not update DOM until start() called', () => {
+      const el = document.createElement('div');
+      new ConstantApp(el, {$screen: 'hello'}, {window: windowStub});
+      expect(el.childNodes).to.have.length(0);
+    });
   });
 
-  it('does not update DOM until start() called', () => {
-    const el = document.createElement('div');
-    new ConstantApp(el, {$screen: 'hello'}, {window: windowStub});
-    expect(el.childNodes).to.have.length(0);
-  });
+  context('with dynamic app state', () => {
+    class StatefulApp extends App {
+      get SCREENS() {
+        return {hello: this.viewFromTemplate(state => h('div.animal', `Hello ${state.animal}`))};
+      }
+    }
 
-  it('injects app state into views', () => {
-    const el = document.createElement('div');
-    new StatefulApp(el, {$screen: 'hello', animal: 'wombat'}, {window: windowStub}).start();
-    expect(el.childNodes).to.have.length(1);
-    const animalEl = el.childNodes[0]
-    expect(animalEl.className).to.eql('animal');
-    expect(animalEl.childNodes[0].data).to.eql('Hello wombat');
+    it('injects app state into views', () => {
+      const el = document.createElement('div');
+      new StatefulApp(el, {$screen: 'hello', animal: 'wombat'}, {window: windowStub}).start();
+      expect(el.childNodes).to.have.length(1);
+      const animalEl = el.childNodes[0]
+      expect(animalEl.className).to.eql('animal');
+      expect(animalEl.childNodes[0].data).to.eql('Hello wombat');
+    });
   });
 
 });
