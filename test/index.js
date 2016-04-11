710b6a8eaa19a3a9f8ddee701c6e74775bfdfbef
t ddddddd <webdev@cmme.org>
2016-04-10 18:25:30 -0700

    DRY out test init a bit

--- test/index.js
+++ test/index.js
@@ -17,16 +17,20 @@ describe('renderer', () => {
       }
     }
 
+    let el, app;
+
+    beforeEach(function() {
+      el = document.createElement('div');
+      app = new ConstantApp(el, {$screen: 'hello'}, {window: windowStub});
+    });
+
     it('updates DOM after start() called', () => {
-      const el = document.createElement('div');
-      new ConstantApp(el, {$screen: 'hello'}, {window: windowStub}).start();
+      app.start();
       expect(el.childNodes).to.have.length(1);
       expect(el.childNodes[0].className).to.eql('hello');
     });
 
     it('does not update DOM until start() called', () => {
-      const el = document.createElement('div');
-      new ConstantApp(el, {$screen: 'hello'}, {window: windowStub});
       expect(el.childNodes).to.have.length(0);
     });
   });
@@ -38,9 +42,15 @@ describe('renderer', () => {
       }
     }
 
+    let el, app;
+
+    beforeEach(function() {
+      el = document.createElement('div');
+      app = new StatefulApp(el, {$screen: 'hello', animal: 'wombat'}, {window: windowStub});
+      app.start();
+    });
+
     it('injects app state into views', () => {
-      const el = document.createElement('div');
-      new StatefulApp(el, {$screen: 'hello', animal: 'wombat'}, {window: windowStub}).start();
       expect(el.childNodes).to.have.length(1);
       const animalEl = el.childNodes[0]
       expect(animalEl.className).to.eql('animal');
