688a4c87b85a1aa196900d0f9fd83c4a8cbcacd5
teddddd <ted@mixpanel.com>
2016-04-14 00:44:32 +0000

    start view tests

---
+++ test/view.js
@@ -0,0 +1,19 @@
+import { View } from '../lib';
+import expect from 'expect.js';
+import h from 'virtual-dom/virtual-hyperscript';
+
+describe('View instance', function() {
+  class SampleView extends View {
+    get TEMPLATE() {
+      return state => h('div.animal', `Hello ${state.animal}`);
+    }
+  }
+
+  it('renders with given state', function() {
+    const view = new SampleView();
+    const vnode = view.render({animal: 'gerbil'});
+    expect(vnode.properties.className).to.eql('animal');
+    expect(vnode.children).to.have.length(1);
+    expect(vnode.children[0].text).to.eql('Hello gerbil');
+  });
+});
