951e59efe342c77de5f692d7140c78aa510d8608
t ddddddd <webdev@cmme.org>
2016-05-11 00:45:16 -0700

    clean out example webpack cruft

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,7 +1,5 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
-var webpack = require('webpack');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -22,20 +20,9 @@ var webpackConfig = {
             presets: ['es2015'],
         },
       },
-      {
-        test: /\.(png|svg)$/,
-        loader: 'url-loader?limit=50000',
-      },
-      {
-        test: /\.styl$/,
-        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!autoprefixer-loader!stylus-loader'),
-      },
     ],
   },
   plugins: [
-    new webpack.DefinePlugin({
-      APP_ENV: JSON.stringify(process.env.NODE_ENV),
-    }),
     new HtmlWebpackPlugin({
       inject: 'head',
       template: 'index.template.html',
