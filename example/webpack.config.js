a193499e0b82d5de8ba66addecf7cecfa99d0d57
t ddddddd <webdev@cmme.org>
2016-05-14 17:07:07 -0700

    excise another useless config line in example

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,8 +1,6 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
 var webpackConfig = {
   entry: './index.js',
   module: {
