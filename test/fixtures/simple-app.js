701fe2baf0ca05698944f678086395cb8b7d38cf
t ddddddd <webdev@cmme.org>
2016-05-17 09:40:47 -0700

    update test fixtures with new config

--- test/fixtures/simple-app.js
+++ test/fixtures/simple-app.js
@@ -2,22 +2,20 @@ import { Component } from '../../lib';
 import h from 'virtual-dom/virtual-hyperscript';
 
 document.registerElement('simple-app', class extends Component {
-  get $defaultState() {
+  get $config() {
     return {
-      foo: 'bar',
-    };
-  }
+      defaultState: {
+        foo: 'bar',
+      },
 
-  get $helpers() {
-    return {
-      capitalize: s => s[0].toUpperCase() + s.slice(1),
-    };
-  }
+      helpers: {
+        capitalize: s => s[0].toUpperCase() + s.slice(1),
+      },
 
-  get $template() {
-    return state => h('.foo', [
-      h('p', `Value of foo: ${state.foo}`),
-      h('p', `Foo capitalized: ${state.$helpers.capitalize(state.foo)}`),
-    ]);
+      template: state => h('.foo', [
+        h('p', `Value of foo: ${state.foo}`),
+        h('p', `Foo capitalized: ${state.$helpers.capitalize(state.foo)}`),
+      ]),
+    };
   }
 });
