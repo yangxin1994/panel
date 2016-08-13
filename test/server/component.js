0b6fc37894ddd9197aa8841f5c9b415c4ec2aca9
t ddddddd <webdev@cmme.org>
2016-08-13 16:09:36 -0700

    test server side class-based component instantiation

--- test/server/component.js
+++ test/server/component.js
@@ -2,11 +2,19 @@ import '../../lib/isorender/dom-shims';
 
 import { expect } from 'chai';
 
-import '../fixtures';
+import { SimpleApp } from '../fixtures/simple-app';
+document.registerElement('simple-app', SimpleApp);
 
 describe('Server-side component renderer', function() {
   it('can register and create components with document.createElement', function() {
     const el = document.createElement('simple-app');
+    expect(el.state).to.eql({});
+    el.attachedCallback();
+    expect(el.state).to.eql({foo: 'bar'});
+  });
+
+  it('supports class instantiation', function() {
+    const el = new SimpleApp();
     expect(el.state).to.eql({});
     el.attachedCallback();
     expect(el.state).to.eql({foo: 'bar'});
