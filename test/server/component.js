a128b01f4a3748d2b789ffe072410788e36bc9e1
t ddddddd <webdev@cmme.org>
2016-08-13 09:34:04 -0700

    make server-side custom element creation shim work

--- test/server/component.js
+++ test/server/component.js
@@ -6,6 +6,8 @@ import '../fixtures';
 describe('Server-side component renderer', function() {
   it('can register and create components with document.createElement', function() {
     const el = document.createElement('simple-app');
+    expect(el.state).to.eql({});
+    el.attachedCallback();
     expect(el.state).to.eql({foo: 'bar'});
   });
 });
