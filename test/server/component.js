a3c4400b4e402713b19e16a0714572acf71efd6b
t ddddddd <webdev@cmme.org>
2016-08-13 18:40:01 -0700

    test server-side updates

--- test/server/component.js
+++ test/server/component.js
@@ -23,7 +23,6 @@ describe('Server-side component renderer', function() {
 
   it('renders a simple component', function(done) {
     const el = new SimpleApp();
-    expect(el.state).to.eql({});
     el.attachedCallback();
     requestAnimationFrame(function() {
       const html = el.innerHTML;
@@ -31,6 +30,21 @@ describe('Server-side component renderer', function() {
       expect(html).to.contain('Value of foo: bar');
       expect(html).to.contain('Foo capitalized: Bar');
       done();
+    });
+  });
+
+  it('renders updates', function(done) {
+    const el = new SimpleApp();
+    el.attachedCallback();
+    requestAnimationFrame(function() {
+      expect(el.textContent).to.contain('Value of foo: bar');
+      expect(el.textContent).to.contain('Foo capitalized: Bar');
+      el.update({foo: 'new value'});
+      requestAnimationFrame(function() {
+        expect(el.textContent).to.contain('Value of foo: new value');
+        expect(el.textContent).to.contain('Foo capitalized: New value');
+        done();
+      });
     });
   });
 });
