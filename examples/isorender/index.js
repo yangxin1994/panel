215dd046af95606ff98c25076d1b4923031e33d5
teddddd <ted@mixpanel.com>
2016-08-30 14:47:48 -0700

    use async/await for isorender example...

--- examples/isorender/index.js
+++ examples/isorender/index.js
@@ -4,6 +4,24 @@ import '../../lib/isorender/dom-shims';
 
 import './counter-app';
 
-const counterApp = document.createElement('counter-app');
-document.createElement('body').appendChild(counterApp);
-requestAnimationFrame(() => console.log(counterApp.outerHTML));
+// promisify requestAnimationFrame for async/await
+const RAF = () => new Promise(requestAnimationFrame);
+
+async function renderAppHTML() {
+  const counterApp = document.createElement('counter-app');
+  document.createElement('body').appendChild(counterApp);
+
+  await RAF();
+
+  console.log('Initial HTML:');
+  console.log(counterApp.outerHTML);
+
+  console.log('Update counter to 5:');
+  counterApp.update({count: 5});
+
+  await RAF();
+
+  console.log(counterApp.outerHTML);
+}
+
+renderAppHTML();
