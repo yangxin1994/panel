c7e33b86404d462e0e7abf117a227b4010da60dc
Andrew Purcell <andrew.purcell@mixpanel.com>
2018-04-11 00:31:27 +0000

    add proxycomponent tests

--- test/browser/component.js
+++ test/browser/component.js
@@ -512,3 +512,38 @@ describe(`Controlled App`, function() {
   });
 });
 
+describe(`ProxyComponent`, function() {
+  let el;
+
+  beforeEach(function(done) {
+    document.body.innerHTML = ``;
+    el = document.createElement(`proxy-app`);
+    el.setAttribute(`data-foo`, `bar`);
+    document.body.appendChild(el);
+    window.requestAnimationFrame(() => done());
+  });
+
+  it(`renders the target element with attributes`, function() {
+    const child = document.querySelector(`event-producer`);
+    expect(child).to.not.eq(null);
+    expect(child.getAttribute(`data-foo`)).to.eq(`bar`);
+  });
+
+  it(`re-dispatches events from child that are not composed`, function(done) {
+    el.addEventListener(`nonComposedEvent`, () => done())
+    el.setAttribute(`send-non-composed`, ``);
+  });
+
+  it(`bubbles composed events`, function(done) {
+    el.addEventListener(`composedEvent`, () => done());
+    el.setAttribute(`send-composed`, ``);
+  });
+
+  it(`re-calculates the target on update`, function() {
+    el.setAttribute(`href`, `#local-link`);
+
+    expect(document.querySelector(`a`)).to.not.eq(null);
+    expect(document.querySelector(`event-producer`)).to.eq(null);
+  });
+});
+
---
+++ test/fixtures/proxy-app.js
@@ -0,0 +1,44 @@
+import {ProxyComponent, Component, h} from '../../lib';
+
+export class ProxyApp extends ProxyComponent {
+  get localConfig() {
+    return {
+      // forces a re-render in test context
+      updateSync: true,
+    };
+  }
+
+  getTargetElement() {
+    // This element will change its behavior if passed an href.
+    return !!this.getAttribute(`href`) ? `a` : `event-producer`;
+  }
+
+  get observedEvents() {
+    return [`nonComposedEvent`, `composedEvent`];
+  }
+
+  static get observedAttributes() {
+    return [`href`].concat(EventProducer.observedAttributes);
+  }
+}
+
+export class EventProducer extends Component {
+  get config() {
+    return {
+      template:() => h(`div`, `I make things happen!`),
+      useShadowDom: true,
+    };
+  }
+
+  attributeChangedCallback(name) {
+    if (name === `send-non-composed`) {
+      this.dispatchEvent(new CustomEvent(`nonComposedEvent`, { bubbles: true, composed: false }));
+    } else if (name === `send-composed`) {
+      this.dispatchEvent(new CustomEvent(`composedEvent`, { bubbles: true, composed: true }));
+    }
+  }
+
+  static get observedAttributes() {
+    return [`send-non-composed`, `send-composed`];
+  }
+}
