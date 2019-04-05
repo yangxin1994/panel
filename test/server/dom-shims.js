7a563ffa9e2a2930a594b422cc594515125b35df
Noj Vek (Manoj P) <nojvek@gmail.com>
2019-04-05 16:03:27 -0700

    isorender support for components with useShadowDom: true (#58)
    * isorender support for components with useShadowDom: true
    
    * ;
    
    * #shadow-root -> shadow-root
    
    * Update test/server/dom-shims.js
    
    Co-Authored-By: nojvek <nojvek@gmail.com>

--- test/server/dom-shims.js
+++ test/server/dom-shims.js
@@ -4,9 +4,12 @@ import '../../lib/isorender/dom-shims';
 
 import {expect} from 'chai';
 
+import nextAnimationFrame from './nextAnimationFrame';
 import {SimpleApp} from '../fixtures/simple-app';
+import {ShadowDomApp} from '../fixtures/shadow-dom-app';
 
 customElements.define(`my-app`, SimpleApp);
+customElements.define(`shadow-dom-app`, ShadowDomApp);
 
 describe(`customElements registry`, function() {
   describe(`.get()`, function() {
@@ -19,5 +22,20 @@ describe(`customElements registry`, function() {
     it(`throws an error if called twice with the same tag name`, function() {
       expect(() => customElements.define(`my-app`, SimpleApp))
         .to.throw(`Registration failed for type 'my-app'. A type with that name is already registered.`); });
+  });
+});
+
+describe(`customElement with shadowDom`, function() {
+  it(`el contains shadow-root`, function() {
+    const elem = document.createElement(`shadow-dom-app`);
+    expect(elem.el.tagName).to.equal(`shadow-root`);
+  });
+
+  it(`renders component`, async function() {
+    const elem = document.createElement(`shadow-dom-app`);
+    elem.connectedCallback();
+    await nextAnimationFrame();
+
+    expect(elem.el.innerHTML).to.equal(`<style>color: blue;</style><div class="foo"><p>Hello</p></div>`);
   });
 });
