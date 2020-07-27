0bab664b3b27247f914d03ba872bbf84300a32fd
Noj <noj.vek@mixpanel.com>
2020-07-27 13:07:00 -0700

    Revert accidental master push

--- test/browser/component.js
+++ test/browser/component.js
@@ -1,6 +1,5 @@
 import {nextAnimationFrame, sleep} from 'domsuite';
 
-import {BreakableApp} from '../fixtures/breakable-app';
 import {compactHtml} from '../utils';
 
 describe(`Simple Component instance`, function () {
@@ -647,13 +646,12 @@ describe(`Rendering exception`, function () {
   });
 
   it(`logs error and emits renderError event`, function () {
-    const errorMessage = `Error while rendering`;
-    expect(el._logError.getCall(0).args[0]).to.equal(errorMessage);
+    const errorMessage = `Error while rendering breakable-app`;
+    expect(el._logError.getCall(0).args[0]).to.contain(errorMessage);
 
     const renderErrorEvent = renderErrorSpy.getCall(0).args[0];
     expect(renderErrorEvent.detail.message).to.contain(errorMessage);
     expect(renderErrorEvent.detail.error).to.be.instanceof(Error);
-    expect(renderErrorEvent.detail.component).to.be.instanceof(BreakableApp);
   });
 
   it(`does not prevent further updates from rendering`, async function () {
